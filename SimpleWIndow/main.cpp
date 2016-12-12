193b149e76cf5be5dd5f13df902ef6544a68045b
Vincent Duong <vduong178@gmail.com>
2016-12-11 18:46:32 -0800

    Initial Commit

---
+++ SimpleWIndow/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SimpleWIndow
+//
+//  Created by Vincent Duong on 12/11/16.
+//  Copyright © 2016 Vincent Duong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
