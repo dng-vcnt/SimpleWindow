7a876c6219719c4bfdae6e15bbe33562869403e6
Vincent Duong <vduong178@gmail.com>
2016-12-11 19:29:58 -0800

    Initial commit

--- SimpleWIndow/main.cpp
+++ SimpleWIndow/main.cpp
@@ -8,8 +8,59 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+
+const GLint WIDTH = 800, HEIGHT = 600;
+
+int main () {
+    glfwInit();
+    
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
+    glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
+    glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE );
+    glfwWindowHint( GLFW_RESIZABLE, GL_FALSE );     // Mac specific. Include for cross-platform
+    
+    GLFWwindow *window = glfwCreateWindow( WIDTH, HEIGHT, "Simple Window", nullptr, nullptr );
+    
+    int screenWidth, screenHeight;
+    glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+    
+    if ( nullptr == window ) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        
+        return -1;
+    }
+    
+    glfwMakeContextCurrent( window );
+    
+    glewExperimental = GL_TRUE;
+    
+    if ( GLEW_OK != glewInit() ) {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+    
+    glViewport( 0, 9, screenWidth, screenHeight );
+    
+    // Create game loop
+    while ( !glfwWindowShouldClose( window ) ) {
+        glfwPollEvents();
+        
+        glClearColor( 0.2f, 0.3f, 0.3f, 1.0f );
+        glClear( GL_COLOR_BUFFER_BIT );
+        
+        glfwSwapBuffers( window );
+    }
+    
+    // When window closes, terminate glfw
+    glfwTerminate();
+    
     return 0;
 }
